6f5e179a4dfd18b352fdccae305b394f2ed88f1c
TurboJessadakorn <105390501+TurboJessadakorn@users.noreply.github.com>
2024-05-29 15:36:31 +0700

    Create types for api data and return data

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+
+export interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    age: number;
+    gender: 'male' | 'female';
+    hair: {
+        color: string;
+    };
+    address: {
+        postalCode: string;
+    };
+    department: string;
+}
+
+export interface TransformedData {
+    [department: string]: {
+        male: number;
+        female: number;
+        ageRange: string;
+        hair: Record<string, number>;
+        addressUser: Record<string, string>;
+    };
+}
